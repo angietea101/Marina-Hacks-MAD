3b2b1038af7d50ea853fcbc3cf39a186ea6a17e1
Maya Escudero <132878327+mayalynnae@users.noreply.github.com>
2024-10-27 08:14:14 -0700

    FIX:aligning

--- my-app/components/homepage/featured.tsx
+++ my-app/components/homepage/featured.tsx
@@ -10,26 +10,28 @@ import Image from "next/image";
 const Features = () => {
     return(
         <div className="container mx-auto">
-            <p className="text-4xl font-bold text-black text-left mb-10">featured restaurants</p>
+            <p className="text-4xl font-bold text-black text-left ">featured restaurants</p>
+            <p className="text-3xl font-regular text-black text-right pb-10 ">browse all</p>
         <div className="ml-7 grid grid-cols-6 gap-8">
-            <div className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pt-7">
+            <button className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0">
                 <Image src={Subway} alt="Subway" height={200} width={200} className="bottom-0 right-0"/>
-            </div>
-            <div className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pt-7 pl-5">
+            </button>
+            <button className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pl-4">
                 <Image src={Chipotle} alt="Chipotle" height={100} width={100} className="bottom-0 right-0"/>
-            </div>
-            <div className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pt-3">
+            </button>
+            <button className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pt-3">
                 <Image src={Dominoes} alt="Dominoes" height={200} width={200} className="bottom-0 right-0"/>
-            </div>
-            <div className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pt-2 pl-5">
+            </button>
+            <button className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pb-2 pl-5">
                 <Image src={Dunkin} alt="Dunkin Donuts" height={100} width={100} className="bottom-0 right-0"/>
-            </div>
-            <div className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pt-8 pl-4">
+            </button>
+            <button className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pl-4">
                 <Image src={Frostbites} alt="Frostbites" height={100} width={100} className="bottom-0 right-0"/>
-            </div>
-            <div className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pt-16 pl-5">
+            </button>
+            <button className="w-[140px] h-[170px] rounded-[100px] shadow-md bg-white hover:shadow-xl hover:bg-secondary border-4 border-secondary z-0 pl-4">
                 <Image src={Cava} alt="Cava" height={100} width={100} className="bottom-0 right-0"/>
-            </div>
+            </button>
+
         </div>
         </div>
     )
