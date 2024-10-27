51030492a2dcbeda8c7cb0b7191782edf77f5eaa
Maya Escudero <132878327+mayalynnae@users.noreply.github.com>
2024-10-27 10:33:44 -0700

    CHORE: filling in pictures

--- my-app/components/homepage/recent.tsx
+++ my-app/components/homepage/recent.tsx
@@ -1,11 +1,8 @@
-import Subway from "@/public/subway.png";
-import Chipotle from "@/public/chipotle1.png";
-import Frostbites from "@/public/frostbites.png";
-import Dominoes from "@/public/dominos.png";
-import Dunkin from "@/public/dunkin.png";
-import Cava from "@/public/cava.png";
 import React from "react";
 import Image from "next/image";
+import Pizza from "@/public/pizzaaa.png"
+import cavaBowl from "@/public/cava_bowl.jpg"
+import Coffee from "@/public/custom_coffee.jpg"
 
 const Recent = () => {
     return(
@@ -13,10 +10,13 @@ const Recent = () => {
             <p className="text-4xl font-bold text-black text-left mt-20 mb-10">recent orders</p>
         <div className="ml-7 grid grid-cols-3 gap-16">
             <div className="w-[250px] h-[290px] rounded-[30px] shadow-md bg-secondary hover:shadow-xl z-0 pt-7">
+            <Image src={Pizza} alt= "Pizza" height={100} width={100} className="bottom-0 right-0"/>
             </div>
             <div className="w-[250px] h-[290px] rounded-[30px] shadow-md bg-secondary hover:shadow-xl z-0 pt-7 pl-5">
+            <Image src={cavaBowl} alt= "cava bowl" height={200} width={200} className="rounded-[28px]"/>
             </div>
             <div className="w-[250px] h-[290px] rounded-[30px] shadow-md bg-secondary hover:shadow-xl z-0 pt-3">
+            <Image src={Coffee} alt= "dunkin coffee" height={200} width={200} className="rounded-[28px] ml-7"/>
             </div>
         </div>
         </div>
